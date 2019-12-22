4b950845b7d178921460da955c4a6eb80240e2cf
Fly-Away <soundgraphia@gmail.com>
2019-12-22 22:14:42 +0700

    CRUD Completed

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('api/v1/posts', include('posts.urls')),
+    path('api/v1/posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
